3830147ee546dd8aca4a72a36b7dc27e68c11d85
Fran Caja <fcaja@socialerp.net>
2013-04-05 10:57:51 +0200

    Update #18

---
+++ SocialTeam/WebFolder/index.waPage/scripts/index.js
@@ -0,0 +1,20 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var Alta_Usuario = {};	// @button
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	Alta_Usuario.click = function Alta_Usuario_click (event)// @startlock
+	{// @endlock
+		$('#frame1').fadeIn('slow');
+		$$('frame1').load('/user/index-tablet.html');
+		
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("Alta_Usuario", "click", Alta_Usuario.click, "WAF");
+// @endregion
+};// @endlock
