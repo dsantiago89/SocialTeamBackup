85768d84c79a4792040e15fd33a16e31f5d21d11
dsantiago89 <dsantiago@telegestion.com>
2013-04-05 09:44:30 +0200

    Update #13 05/04/2013 - 09:44

---
+++ SocialTeam/WebFolder/user.waPage/scripts/index-tablet.js
@@ -0,0 +1,23 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var btn_save = {};	// @button
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	btn_save.click = function btn_save_click (event)// @startlock
+	{// @endlock
+		var usuario = $$('txt_user').getValue();
+		var fullname = $$('txt_fullname').getValue();
+		var password = $$('txt_pass').getValue();
+		var grupo = $$('sl.group').getValue();
+		
+		directoryComponent.addUser
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("btn_save", "click", btn_save.click, "WAF");
+// @endregion
+};// @endlock
