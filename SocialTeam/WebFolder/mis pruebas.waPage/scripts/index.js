d6435333286b606604d5e0cd5cc64dbb3fe7d9d3
José Miguel Soriano Catalá <jmsoria@MacBook-Negro.local>
2013-04-08 11:28:11 +0200

    Actualización

---
+++ SocialTeam/WebFolder/mis pruebas.waPage/scripts/index.js
@@ -0,0 +1,37 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var button2 = {};	// @button
+	var button1 = {};	// @button
+	var documentEvent = {};	// @document
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	button2.click = function button2_click (event)// @startlock
+	{// @endlock
+		var1--;
+		alert(var1);
+	};// @lock
+
+	button1.click = function button1_click (event)// @startlock
+	{// @endlock
+		var1++;
+		alert (var1);
+		SSS++;
+		alert (SSS);
+	};// @lock
+
+	documentEvent.onLoad = function documentEvent_onLoad (event)// @startlock
+	{// @endlock
+		var1=0;
+		SSS=2;
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("button2", "click", button2.click, "WAF");
+	WAF.addListener("button1", "click", button1.click, "WAF");
+	WAF.addListener("document", "onLoad", documentEvent.onLoad, "WAF");
+// @endregion
+};// @endlock
